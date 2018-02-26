8d9f43eb1a2ba3ba052bdd6def5882a609ddc5d5
karthrand <735867550@qq.com>
2018-02-26 20:18:03 +0800

    新增返回传递

--- app/src/main/java/com/oudedemo/passarg/MainActivity.java
+++ app/src/main/java/com/oudedemo/passarg/MainActivity.java
@@ -11,16 +11,17 @@ public class MainActivity extends Activity
     private Button button;
     private EditText et1,et2;
     private String name,test;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setContentView(R.layout.main);
         //对象绑定
         button=(Button) findViewById(R.id.mainButton1);
         et1=(EditText) findViewById(R.id.mainEditText1);
         et2=(EditText) findViewById(R.id.mainEditText2);
+        tv=(TextView) findViewById(R.id.mainTextView2);
         
         //button实现点击事件
         button.setOnClickListener(new View.OnClickListener(){
@@ -56,13 +57,34 @@ public class MainActivity extends Activity
                     bundle.putInt("numbers",number);
                     intent.putExtras(bundle);
                     
-                    //跳转
+                    //跳转,1为请求码，保证为唯一值就行
                     intent.setClass(MainActivity.this,TargetActivity.class);
-                    startActivity(intent);
-                    MainActivity.this.finish();
+                    startActivityForResult(intent,1);
+                    
                     
                 }
             });
-                     
+            
+                    
     }
+     /* 步骤六 接收返回的信息  */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+       switch(requestCode){
+           case 1:
+               if (resultCode == RESULT_OK){
+                   String result = data.getStringExtra("result");
+                   tv.setText(result);
+               }
+               break;
+           default:
+       }
+       
+        
+    }
+
+
+    
+    
 }
--- app/src/main/java/com/oudedemo/passarg/TargetActivity.java
+++ app/src/main/java/com/oudedemo/passarg/TargetActivity.java
@@ -4,10 +4,13 @@ import android.app.*;
 import android.os.*;
 import android.content.*;
 import android.widget.*;
+import android.view.*;
 
 public class TargetActivity extends Activity
 {
     private TextView tx;
+    private EditText et;
+    private Button bn;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -15,16 +18,15 @@ public class TargetActivity extends Activity
         //此处为指定对应的布局yaml
         setContentView(R.layout.target);
         tx=(TextView) findViewById(R.id.targetTextView1);
+        et=(EditText) findViewById(R.id.targetEditText1);
+        bn=(Button) findViewById(R.id.targetButton1);
         
         /*  步骤四：接收参数并在TextView中显示  */
         /* 直接intent方式
          Intent  intent = getIntent();
          String  name   = intent.getStringExtra("name");
          Integer number = intent.getIntExtra("numbers",0);
-        
         */
-        
-        
         //bundle+intent方式
         Intent  intent = getIntent();
         Bundle  bundle = intent.getExtras();
@@ -35,5 +37,26 @@ public class TargetActivity extends Activity
         tx.setText("name:"+name+","+"number:"+number);
         
         /*  步骤四结束  */
+        
+        /* 步骤5 点击按钮后返回输入框内的信息   */
+        bn.setOnClickListener(new View.OnClickListener(){
+                private String result;
+
+                @Override
+                public void onClick(View p1)
+                {
+                    
+                }
+            });
+    }
+    /*  也是步骤五：防止用户使用回退键返回  */
+    @Override
+    public void onBackPressed()
+    {
+        String result = et.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("result",result);
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
